1835905600eaf70c1f69f04af8038485d6d4f741
Cecilia Lobo <cecilialobo92@gmail.com>
2021-02-21 20:26:33 +0100

    Add the solution to the last test

--- gigasecond/gigasecond.js
+++ gigasecond/gigasecond.js
@@ -1,4 +1,5 @@
-export const gigasecond = (newDate) => {
-  newDate.setUTCSeconds(newDate.getSeconds() + 1000000000 );
-  return newDate;
+export const gigasecond = (date) => {
+  const dateToModify = date.getTime();
+  const dateWithGigaseconds = new Date(dateToModify + 1000000000000);
+  return dateWithGigaseconds;
 };
