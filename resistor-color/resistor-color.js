92ad0ad2da442ba7f1ac65be5e43b065938b4dd0
Cecilia Lobo <cecilialobo92@gmail.com>
2021-02-21 20:18:57 +0100

    Refactoring

--- resistor-color/resistor-color.js
+++ resistor-color/resistor-color.js
@@ -1,18 +1,16 @@
 export const colorCode = (color) => {
-  return resistorColor[color];
+  return COLORS.indexOf(color);
 };
 
-export const resistorColor = {
-  'black': 0,
-   'brown': 1,
-   'red': 2,
-   'orange': 3,
-   'yellow': 4,
-   'green': 5,
-   'blue': 6,
-   'violet': 7,
-   'grey': 8,
-   'white': 9
-};
-
-export const COLORS = Object.keys(resistorColor);
+export const COLORS = [
+  'black',
+   'brown',
+   'red',
+   'orange',
+   'yellow',
+   'green',
+   'blue',
+   'violet',
+   'grey',
+   'white'
+];
